d764b023938707ae8993e52e84b1f113846652d4
wushurui <wsr0727@qq.com>
2019-12-02 18:18:41 +0800

    文件处理3

--- File_Course/File_Operation.py
+++ File_Course/File_Operation.py
@@ -15,27 +15,26 @@ class FileOperation:
                 new_name = os.path.join(root, 'wsr' + f)
                 os.rename(file_path, new_name)
                 file_new_name.append(new_name)
+
         return file_new_name
     def file_sort(self):
         # 获取文件创建时间,并写入字典
         file_sort = {}
         for f in self.file_rename():
             file_sort[f] = os.path.getctime(f)
+        return file_sort
 
-        for k in sorted(file_sort, key=file_sort.__getitem__):
-            return k
 
     def file_rename2(self):
         #从后往前数 第一个小数点前加序号
-
-        for root, dirs, files in os.walk(self.file_dir):
-            num = 1
-            for f in files:
-                file_path = os.path.join(root, f)
-                new_name = os.path.join(root, str(num) + f)
-                os.rename(file_path, new_name)
-                num += 1
+        num = 1
+        for k in sorted(self.file_sort(), key=self.file_sort().__getitem__):
+            strk =
+            new_name = os.path.join()
+            os.rename(k, new_name)
+            num += 1
 
 
 x = FileOperation('./1')
+print(x.file_rename())
 print(x.file_sort())
