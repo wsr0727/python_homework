bd61ca9ecfdec7eeb604ee4fcf5f6cb37263b1b2
wushurui <wsr0727@qq.com>
2019-12-03 22:01:07 +0800

    更新简化dir命令

---
+++ simple_dir.py
@@ -0,0 +1,29 @@
+import os
+import time
+
+def simple_dir(file_dir):
+    '''
+    输出路径下的所有文件信息
+    :return: none
+    '''
+    file_num = 0
+    dir_num = 0
+    file_size = 0
+    for f in os.listdir(file_dir):
+        file_path = os.path.join(file_dir, f)
+        time_local = time.localtime(os.path.getctime(file_path))
+        file_time = time.strftime("%Y/%m/%d  %a %H:%M:%S", time_local)
+        if os.path.isdir(file_path) == True:
+            file_isdir = '<DIR>'
+            dir_num += 1
+        else:
+            file_isdir = os.path.getsize(file_path)
+            file_size = file_size + file_isdir
+            file_num += 1
+        print(file_time, file_isdir, f)
+    print(file_num, '个文件   ', file_size, '字节')
+    print(dir_num, '个目录')
+
+
+if __name__ == '__main__':
+    simple_dir(input("文件路径："))
